de39963cb7087327cc3e93406977ca75d5a24b87
DamianAf <damian.afanasii01@gmail.com>
2026-04-08 11:50:03 +0100

    i dont remember

--- fish/functions/pls.fish
+++
@@ -1,3 +0,0 @@
-function pls --wraps='sudo alias --save pls "sudo $history[1]"' --description 'alias pls sudo alias --save pls "sudo $(fc -ln -1)"'
-    sudo alias --save pls "sudo $history[1]" $argv
-end
---
+++ fish/functions/start.fish
@@ -0,0 +1,3 @@
+function start --wraps='niri-session -l' --description 'alias start niri-session -l'
+    niri-session -l $argv
+end
